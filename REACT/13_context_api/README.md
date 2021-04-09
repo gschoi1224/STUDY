b8563ca0f81d7571ede3bf94f68610d5193c499b
CGS <gschoi1224@gmail.com>
2021-04-09 14:46:07 +0900

    use static contextType

--- REACT/13_context_api/README.md
+++ REACT/13_context_api/README.md
@@ -141,3 +141,32 @@ return (
 
 -   클래스형 컴포넌트에서 Context를 좀 더 쉽게 사용하고 싶다면 static contextType을 정의하는 방법
 -   SelectColors 컴포넌트를 클래스형으로 변환하기
+-   static contextType을 정의하면 클래스 메서드에서도 Context에 넣어 둔 함수를 호출할 수 있다는 장점이 있음
+-   한 클래스에서 하나의 Context밖에 사용하지 못한다는 단점이 있음
+-   앞으로 새로운 컴포넌트를 작성할 때 클래스형으로 작성하는 일은 많지 않기 때문에 useContext를 사용하는 쪽을 권장
+
+```js
+class SelectColors extends Component {
+    static contextType = ColorContext;
+
+    handleSetColor = color => {
+        this.context.actions.setColor(color);
+    };
+
+    handleSetSubcolor = subcolor => {
+        this.context.actions.setSubcolor(subcolor);
+    };
+    render() {
+        return (
+            ...
+            <div onClick={() => this.handleSetColor(color)}
+            onContextMenu={e => {
+                e.preventDefault();
+                this.hnadleSetSubcolor(color);
+            }}
+            />
+            ...
+        )
+    }
+}
+```
--- REACT/13_context_api/src/components/SelectColors.js
+++ REACT/13_context_api/src/components/SelectColors.js
@@ -1,8 +1,34 @@
-import React from 'react';
-import { ColorConsumer } from '../contexts/color';
+import React, { Component } from 'react';
+import ColorContext, { ColorConsumer } from '../contexts/color';
 
 const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
 
+class SelectColors extends Component {
+    static contextType = ColorContext;
+    render() {
+        return (
+            <div>
+                <h2>색상을 선택하세요.</h2>
+                <div style={{ display: 'flex' }}>
+                    {colors.map(color => (
+                        <div
+                            key={color}
+                            style={{
+                                background: color,
+                                width: '24px',
+                                height: '24px',
+                                cursor: 'pointer',
+                            }}
+                        />
+                    ))}
+                    <hr />
+                </div>
+            </div>
+        );
+    }
+}
+
+/*
 const SelectColors = () => {
     return (
         <div>
@@ -33,5 +59,5 @@ const SelectColors = () => {
         </div>
     );
 };
-
+*/
 export default SelectColors;
