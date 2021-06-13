020e695cdd1e99457ca6107d4572a2d4963d0280
gschoi1224 <66709937+gschoi1224@users.noreply.github.com>
2021-06-13 22:15:05 +0900

    쓸데없는거 print 찍고있는지 확인하기

--- PYTHON/6_DYNAMIC_PROGRAMMING/q3.py
+++ PYTHON/6_DYNAMIC_PROGRAMMING/q3.py
@@ -20,7 +20,6 @@ for _ in range(n) :
 # 리스트를 뒤에서부터 거꾸로 확인
 for i in range(n - 1, -1, -1) :
     time = t[i] + i
-    print(time)
     # 상담이 기간 안에 끝나는 경우
     if time <= n :
         # 점화식에 맞게, 현재까지의 최고 이익 계산
@@ -29,5 +28,4 @@ for i in range(n - 1, -1, -1) :
     # 상담이 기간을 벗어나는 경우
     else :
         dp[i] = max_value
-print(dp)
 print(max_value)
