3f65dc427b1cc8dc6dbb9c47f0a2283a71f40752
CGS <gschoi1224@gmail.com>
2021-08-03 17:59:37 +0900

    알고리즘

---
+++ ALGORITHM/PROGRAMMERS/stackQueue/42586.py
@@ -0,0 +1,24 @@
+import math
+def solution(progresses, speeds) :
+    answer = []
+    day = 0
+    cnt = 0
+    for i in range(len(progresses)) :
+        remain = 100 - progresses[i]
+        days = math.trunc(remain / speeds[i])
+        if cnt == 0 :
+            cnt += 1
+            day = days
+        elif days <= day :
+            cnt += 1
+        elif days > day :
+            answer.append(cnt)
+            day = days
+            cnt = 1
+        if i == len(progresses) - 1 :
+            answer.append(cnt)
+    return answer
+
+
+print(solution([93, 30, 55], [1, 30, 5]))  # [2, 1]
+print(solution([95, 90, 99, 99, 80, 99], [1, 1, 1, 1, 1, 1])) # [1, 3, 2]
