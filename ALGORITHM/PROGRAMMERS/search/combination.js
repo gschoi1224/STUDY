96098ed4453534e3be2cfc00f8667a41a274e680
CGS <gschoi1224@gmail.com>
2021-08-06 15:05:05 +0900

    조합 함수

---
+++ ALGORITHM/PROGRAMMERS/search/combination.js
@@ -0,0 +1,17 @@
+function combination(arr, num) {
+    const result = [];
+    if (num === 1) return arr.map(a => [a]);
+    arr.forEach((a, i) => {
+        const fixed = a;
+        const rest = arr.filter((r, idx) => idx !== i);
+        const combined = combination(rest, num - 1);
+        combined.forEach(c => {
+            result.push([fixed, ...c]);
+        });
+    });
+    return result;
+}
+
+console.log(combination([1, 2, 3], 2));
+
+// 순열(combination)은 뽑은 다음에 순서를 생각해야 하지만 조합(combination)은 뽑기만 해도 됨
