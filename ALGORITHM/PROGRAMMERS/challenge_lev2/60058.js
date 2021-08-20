4e9cd7560050579ab1ad571a7a8d4f5de0cc6afb
gschoi1224 <66709937+gschoi1224@users.noreply.github.com>
2021-08-20 22:35:37 +0900

    2020 KAKAO BLIND RECRUITMENT > 괄호 변환

--- ALGORITHM/PROGRAMMERS/challenge_lev2/60058.js
+++ ALGORITHM/PROGRAMMERS/challenge_lev2/60058.js
@@ -1,25 +1,53 @@
 function solution(p) {
-    var answer = '';
+    return division(p.split('')).join('');
+}
+
+function isValance(arr) {
     let left = 0;
     let right = 0;
-    p = p.split('');
-    let u = '';
-    let v = '';
-    for (let i = 0; i < p.length; i++) {
-        let c = p[i];
-        if (c === '(') left++;
+    let result = true;
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === '(') left++;
         else right++;
-        if (left === right) {
-            u = p.slice(0, i + 1);
-            v = p.slice(i + 1);
+        if (left < right) {
+            result = false;
             break;
         }
     }
-    console.log(p, u, v);
-    return p.join('');
+    return result;
 }
 
-function isValance(str) {}
+function division(arr) {
+    let left = 0;
+    let right = 0;
+    let u = [],
+        v = [];
+    const result = [];
+    for (let i = 0; i < arr.length; i++) {
+        let c = arr[i];
+        if (c === '(') left++;
+        else right++;
+        if (left === right) {
+            u = arr.slice(0, i + 1);
+            v = arr.slice(i + 1);
+            if (isValance(u)) {
+                result.push(...u);
+                result.push(...division(v));
+            } else {
+                result.push('(');
+                result.push(...division(v));
+                result.push(')');
+                result.push(
+                    ...u
+                        .filter((_, idx) => idx > 0 && idx < u.length - 1)
+                        .map(s => (s === '(' ? ')' : '(')),
+                );
+            }
+            break;
+        }
+    }
+    return result;
+}
 
 console.log(solution('(()())()')); // "(()())()"
 console.log(solution(')(')); // "()"
