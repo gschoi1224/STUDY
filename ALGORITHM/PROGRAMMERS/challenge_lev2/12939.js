2aa389adb299976eb7d7fd24a4c586eeefd64b6a
CGS <gschoi1224@gmail.com>
2021-09-02 15:46:21 +0900

    연습문제 > 최댓값과 최솟값
    Math.max 문자열로 알아서 파싱함..ㅋ

---
+++ ALGORITHM/PROGRAMMERS/challenge_lev2/12939.js
@@ -0,0 +1,6 @@
+const solution = s =>
+    [Math.min(...s.split(' ')), Math.max(...s.split(' '))].join(' ');
+
+console.log(solution('1 2 3 4')); // "1 4"
+console.log(solution('-1 -2 -3 -4')); // "-4 -1"
+console.log(solution('-1 -1')); // "-1 -1"
