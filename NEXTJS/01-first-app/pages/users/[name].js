fe3daa68bf804db371e1a0cfe51e8504a96cb800
gschoi1224 <66709937+gschoi1224@users.noreply.github.com>
2021-09-21 23:39:02 +0900

    Next.js 연습

---
+++ NEXTJS/01-first-app/pages/users/[name].js
@@ -0,0 +1,30 @@
+import fetch from 'isomorphic-unfetch';
+import Profile from '../../components/profile';
+
+const name = ({ user }) => {
+  if (!user) {
+    return null;
+  }
+  return (
+    <>
+      <Profile user={user} />
+    </>
+  );
+};
+
+export const getServerSideProps = async ({ query }) => {
+  const { name } = query;
+  try {
+    const res = await fetch(`https://api.github.com/users/${name}`);
+    if (res.status === 200) {
+      const user = await res.json();
+      return { props: { user } };
+    }
+    return { props: {} };
+  } catch (e) {
+    console.log(e);
+    return { props: {} };
+  }
+};
+
+export default name;
