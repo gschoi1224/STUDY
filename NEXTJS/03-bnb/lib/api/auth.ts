1d85b4e79ae17bf392a701e66bc435403a0e61d4
CGS <gschoi1224@gmail.com>
2021-10-14 11:00:39 +0900

    로그인 유지 기능 구현
    헤더의 cookie에서 jwt토큰 확인해 있으면 바로 redux에 저장해 로그인

--- NEXTJS/03-bnb/lib/api/auth.ts
+++ NEXTJS/03-bnb/lib/api/auth.ts
@@ -1,4 +1,4 @@
-import axios from 'axios';
+import axios from './';
 import { UserType } from '../../types/user';
 
 // 회원가입 body
@@ -15,3 +15,6 @@ export const signupAPI = body => axios.post<UserType>('/api/auth/signup', body);
 
 // 로그인 api
 export const loginAPI = body => axios.post<UserType>('/api/auth/login', body);
+
+// 쿠키의 access_token의 유저 정보 받아오는 api
+export const meAPI = () => axios.get<UserType>('/api/auth/me');
--- NEXTJS/03-bnb/pages/_app.tsx
+++ NEXTJS/03-bnb/pages/_app.tsx
@@ -1,7 +1,11 @@
-import { AppProps } from 'next/app';
+import axios from '../lib/api';
+import App, { AppProps, AppContext } from 'next/app';
 import Header from '../components/Header';
+import { cookieStringToObject } from '../lib/utils';
 import { wrapper } from '../store';
 import GlobalStyle from '../styles/GlobalStyle';
+import { meAPI } from '../lib/api/auth';
+import { userActions } from '../store/user';
 
 const app = ({ Component, pageProps }: AppProps) => {
     return (
@@ -13,5 +17,26 @@ const app = ({ Component, pageProps }: AppProps) => {
         </>
     );
 };
+
+app.getInitialProps = wrapper.getInitialAppProps(
+    store => async (context: AppContext) => {
+        const appInitialProps = await App.getInitialProps(context);
+        const cookieObject = cookieStringToObject(
+            context.ctx.req?.headers.cookie,
+        );
+        const { isLogged } = store.getState().user;
+        try {
+            // 리덕스 스토어의 isLogged가 true이거나 access_token이 없다면 api 보내지 않음
+            if (!isLogged && cookieObject.access_token) {
+                axios.defaults.headers.cookie = cookieObject.access_token;
+                const { data } = await meAPI();
+                store.dispatch(userActions.setLoggedUser(data));
+            }
+        } catch (e) {
+            console.log(e);
+        }
+        return { ...appInitialProps };
+    },
+);
 
 export default wrapper.withRedux(app);
